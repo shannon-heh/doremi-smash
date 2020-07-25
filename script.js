bf9fe0f0f462fa9cef70d46df2c1d9f6015a98cb
Claire Chang <claires.chang@gmail.com>
2020-07-25 14:49:25 -0400

    add start button and stop when game over

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
+var ingame = false;
+
 function jump(){
     if(character.classList == "animate"){return}
     character.classList.add("animate");
@@ -8,16 +10,26 @@ function jump(){
         character.classList.remove("animate");
     },300);
 }
+
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    if(ingame) {
+        if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+            block.style.animation = "none";
+            // alert("Game Over. score: "+Math.floor(counter/100));
+            counter=0;
+            character.classList.remove("animate");
+            ingame = false;
+        }else{
+            counter++;
+            document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        }
     }
 }, 10);
+
+function startgame() {
+    ingame = true;
+    block.style.animation = "block 1s infinite linear";
+    // character.classList.add("animate"); 
+}
