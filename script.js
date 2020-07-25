c44c17f82ba9a5413b483d55eced818a56eb6963
Shannon Heh <shannon.heh@princeton.edu>
2020-07-25 13:08:50 -0700

    added L/R functionality

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
+
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+// handles when key is pushed
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+// handles when key is lifted
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+var ingame = false;
+var concrete = document.getElementById("break-concrete");
+
 function jump(){
     if(character.classList == "animate"){return}
     character.classList.add("animate");
@@ -8,16 +37,49 @@ function jump(){
         character.classList.remove("animate");
     },300);
 }
+
+var offset=0;
 var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    // console.log(window.getComputedStyle(character));
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top")); 
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+
+    if(ingame) {
+        if(rightPressed) {
+            character.style.left = offset+'px';
+            offset += 1;
+            characterWidth = window.getComputedStyle(character).getPropertyValue("width");
+            console.log(characterWidth);
+            if(offset>700) {
+                offset=700;
+                console.log(offset);
+            }
+        }
+        if(leftPressed) {
+            character.style.left = offset+'px';
+            offset -= 1;
+            if(offset<0) {
+                offset=0;
+            }
+        }
+
+        if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+            block.style.animation = "none";
+            alert("Game Over. score: "+Math.floor(counter/100));
+            counter=0;
+            character.classList.remove("animate");
+            concrete.innerHTML = "Play the right note to break out this musician!";
+            ingame = false;
+        }else{
+            counter++;
+            document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        }
     }
 }, 10);
+
+function startgame() {
+    ingame = true;
+    block.style.animation = "block 2s infinite linear";
+    concrete.innerHTML = "";
+    // character.classList.add("animate"); 
+}
